52706ad3730c445aa892e3248c7b37c0e2d4776e
Mikle <michaels17525@mail.ru>
2020-09-23 14:19:38 +0300

    Add some CRUD elements with redirection

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 # Create your views here.
@@ -11,3 +11,8 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
     context_object_name = "post"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
